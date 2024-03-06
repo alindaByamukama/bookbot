384b77c4ecf8559c9b42cd93ff234bd88bb4a441
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-06 21:42:04 +0300

    add function that counts letters and returns a dict char -> char-count

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     word_count_str = str(num_of_words(file_contents))
+    char_count_dict = count_letters(file_contents)
     print(word_count_str)
+    print(char_count_dict)
     
 def num_of_words(text):
     words = len(text.split())
     return words
+
+def count_letters(text):
+    lowercase_text = text.lower().split()
+    chars = "".join(lowercase_text)
+    chars_dict = {}
+    for char in chars:
+        if char in chars_dict:
+            chars_dict[char] += 1   
+        else:
+            chars_dict[char] = 1   
+    return chars_dict
 
 if __name__ == '__main__':
     main()
